6525e6cb5c49afe01db61add07c81713fee534cc
robertm723 <robertmagomero@gmail.com>
2019-04-06 02:11:46 -0400

    database changes

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@
 
 
 if(process.env.NODE_ENV === 'production'){
-    module.exports = {mongoURI: 'mongodb+srv://rob:rob@djprod-lyyjp.mongodb.net/test?retryWrites=true'
+    module.exports = {mongoURI: 'mongodb+srv://rob:wiseguy723@djprod-lyyjp.mongodb.net/test?retryWrites=true'
 }
 }else{
     module.exports = {mongoURI: 'mongodb://localhost/diary-dev'}

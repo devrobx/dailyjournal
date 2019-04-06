ed18014e72739bbb18809010ab0bc2a2960dd71f
robertm723 <robertmagomero@gmail.com>
2019-04-06 14:43:00 -0400

    database

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@
 
 
 if(process.env.NODE_ENV === 'production'){
-    module.exports = {mongoURI: 'mongodb://rob:wiseguy723@ds111025.mlab.com:11025/heroku_j29r2bns'
+    module.exports = {mongoURI: process.env.MONGODB_URI
 }
 }else{
     module.exports = {mongoURI: 'mongodb://localhost/diary-dev'}
